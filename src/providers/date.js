fae4848f44aa7c2340709b8b6ffc2fc6227204ec
estelapedrero <estelapedrero@gmail.com>
2017-09-24 22:18:12 +0200

    momentjs added

--- src/providers/date.js
+++ src/providers/date.js
@@ -1,9 +1,5 @@
 import moment from 'moment';
 
 export default {
-  ago: (date = new Date()) => moment(date).fromNow(),
-  html: (date = new Date()) => moment(date).format('YYYY-MM-DD'),
-  time: (date = new Date()) => moment(date).format('LT'),
   short: (date = new Date()) => moment(date).format('DD.MM.YYYY'),
-  verbose: (date = new Date()) => moment(date).format('ll'),
 };
