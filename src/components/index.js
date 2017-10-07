875e97f299f633b63cbeb00d55d4a22b66ee2462
estelapedrero <estelapedrero@gmail.com>
2017-10-07 21:34:48 +0200

    svg icon remaned

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ export { default as Footer } from './Footer';
 export { default as Form } from './Form';
 export { default as Graphic } from './Graphic';
 export { default as Header } from './Header';
-export { default as Icon } from './Icon';
 export { default as Link } from './Link';
+export { default as Svgicon } from './Svgicon';
 export { default as Switch } from './Switch';
 export { default as Table } from './Table';
